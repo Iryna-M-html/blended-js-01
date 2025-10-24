7b052736d6dc08f22ef470797ac9ec42422c0a3e
Iryna-M-html <ivmohnataya@gmail.com>
2025-10-24 15:28:16 +0300

    fetch

--- js/http.js
+++ js/http.js
@@ -1,26 +1,66 @@
 // https://jsonplaceholder.typicode.com/todos
-const list = document.querySelector(".todo-list");
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => {
-    //   console.log(res);
-    if (!res.ok) {
-      throw new Error("OOps");
-    }
-    return res.json();
-  })
-  .then((data) => {
-    console.log("then", data);
-    list.insertAdjacentHTML("beforeend", createMarkup(data));
-  })
-  .catch((error) => {
-    console.log("catch", error);
-  });
+
+//количество на странице
+// отображение конскретной стрвницы
+// обработка запроса (промиса)
+// const list = document.querySelector(".todo-list");
+// const params = new URLSearchParams({
+//   _limit: 5,
+//   _page: 1,
+// });
+
+// fetch(`https://jsonplaceholder.typicode.com/todos?${params}`)
+//   .then((res) => {
+//     //   console.log(res);
+//     if (!res.ok) {
+//       throw new Error("OOps");
+//     }
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log("then", data);
+//     list.insertAdjacentHTML("beforeend", createMarkup(data));
+//   })
+//   .catch((error) => {
+//     console.log("catch", error);
+//   });
+
+// fetch(`https://jsonplaceholder.typicode.com/users`)
+//   .then((res) => {
+//     //   console.log(res);
+//     if (!res.ok) {
+//       throw new Error("OOps");
+//     }
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log("users", data);
+//     list.insertAdjacentHTML("beforeend", createMarkup(data));
+//   })
+//   .catch((error) => {
+//     console.log("catch", error);
+//   });
+
+// function createMarkup(todos) {
+//   return todos
+//     .map(
+//       ({ id, title, completed }) => `
+//     <li data-id="${id}" class="list-item">
+//         <input type="checkbox"${completed && "checked"}/>
+//         <p>${title}</p>
+//     </li>
+//     `
+//     )
+//     .join("");
+// }
+
+//////
 
 function createMarkup(todos) {
   return todos
     .map(
       ({ id, title, completed }) => `
-    <li data-id="${id}" class="list-item>
+    <li data-id="${id}" class="list-item">
         <input type="checkbox"${completed && "checked"}/>
         <p>${title}</p>
     </li>
@@ -28,3 +68,27 @@ function createMarkup(todos) {
     )
     .join("");
 }
+
+function foo(url) {
+  return fetch(url).then((result) => {
+    if (!result.ok) {
+      throw new Error(result.status);
+    }
+    return result.json();
+  });
+}
+
+foo(`https://jsonplaceholder.typicode.com/todos`)
+  .then((data) => {
+    list.insertAdjacentHTML("beforeend", createMarkup(data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+foo(`https://jsonplaceholder.typicode.com/users`)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
